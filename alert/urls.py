264d0c497e421f47e30efd9d9e286728b3177c24
songxitang <877646746@qq.com>
2019-01-30 09:46:20 +0800

    test

--- alert/urls.py
+++ alert/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # path('<int:question_id>/results/', views.results, name='results'),
     # path('<int:question_id>/vote/', views.vote, name="vote")
     path('', views.index, name='index'),
-    path('/node_alert', views.node_alert, name='node_alert'),
-    path('/pod_alert', views.pod_alert, name='pod_alert')
+    path('node_alert', views.node_alert, name='node_alert'),
+    path('pod_alert', views.pod_alert, name='pod_alert')
 ]
--- alert/views.py
+++ alert/views.py
@@ -1,15 +1,32 @@
+import json
+
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 
 def index(request):
-    pass
+    if request.method == "POST":
+        concat = request.POST
+        post_body = request.body
+        json_body = json.loads(post_body)
+        print("concat: ", concat)
+        print("json_body: ", json_body)
 
 
 def node_alert(request):
-    pass
+    if request.method == "POST":
+        concat = request.POST
+        post_body = request.body
+        json_body = json.loads(post_body)
+        print("concat: ", concat)
+        print("json_body: ", json_body)
 
 
 def pod_alert(request):
-    pass
+    if request.method == "POST":
+        concat = request.POST
+        post_body = request.body
+        json_body = json.loads(post_body)
+        print("concat: ", concat)
+        print("json_body: ", json_body)
