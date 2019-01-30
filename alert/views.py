105c6b3217ace4b855c85bb866bbc19a95ff37d8
songxitang <877646746@qq.com>
2019-01-30 11:25:16 +0800

    添加 json response

--- alert/views.py
+++ alert/views.py
@@ -7,29 +7,23 @@ from django.http import HttpResponse, JsonResponse
 
 def index(request):
     if request.method == "POST":
-        concat = request.POST
         post_body = request.body
         json_body = json.loads(post_body)
-        # print("concat: ", concat)
-        # print("json_body: ", json_body)
-        return JsonResponse({"concat": concat, "json_body": json_body}, safe=False)
+        print("json_body: ", json_body)
+        return JsonResponse({"json_body": json_body}, safe=False)
 
 
 def node_alert(request):
     if request.method == "POST":
-        concat = request.POST
         post_body = request.body
         json_body = json.loads(post_body)
-        # print("concat: ", concat)
-        # print("json_body: ", json_body)
-        return JsonResponse({"concat": concat, "json_body": json_body}, safe=False)
+        print("json_body: ", json_body)
+        return JsonResponse({"json_body": json_body}, safe=False)
 
 
 def pod_alert(request):
     if request.method == "POST":
-        concat = request.POST
         post_body = request.body
         json_body = json.loads(post_body)
-        # print("concat: ", concat)
-        # print("json_body: ", json_body)
-        return JsonResponse({"concat": concat, "json_body": json_body}, safe=False)
+        print("json_body: ", json_body)
+        return JsonResponse({"json_body": json_body}, safe=False)
