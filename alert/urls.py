788d160de07c2f0b7ec9ef68e25e1de08a30a477
songxitang <877646746@qq.com>
2019-01-30 09:32:47 +0800

    Initial commit

---
+++ alert/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'alert'
+urlpatterns = [
+    # path('', views.index, name='index'),
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # path('<int:question_id>/vote/', views.vote, name="vote")
+    path('', views.index, name='index'),
+    path('/node_alert', views.node_alert, name='node_alert'),
+    path('/pod_alert', views.pod_alert, name='pod_alert')
+]
---
+++ alert/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def index(request):
+    pass
+
+
+def node_alert(request):
+    pass
+
+
+def pod_alert(request):
+    pass
